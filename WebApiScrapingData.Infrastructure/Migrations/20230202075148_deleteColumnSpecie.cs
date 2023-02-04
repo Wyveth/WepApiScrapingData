b09903fde8cb6e053ed7d905e04fd4b550e13791
Garrigue Benjamin <93991961+Wyveth@users.noreply.github.com>
2023-02-04 16:50:50 +0100

    Delete column species

---
+++ WebApiScrapingData.Infrastructure/Migrations/20230202075148_deleteColumnSpecie.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiScrapingData.Infrastructure.Migrations
+{
+    public partial class deleteColumnSpecie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Specie",
+                table: "DataInfos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Specie",
+                table: "DataInfos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
